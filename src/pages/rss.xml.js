f47d907b35c58409eaa7a29423e4589919a6087f
jtenner <tenner.joshua@gmail.com>
2024-12-20 22:56:08 -0500

    fix rss generation

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -11,6 +11,8 @@ export async function GET(context) {
     items: posts.map((post) => ({
       ...post.data,
       link: `/blog/${post.id}/`,
+      pubDate: post.date,
+      description: post.description ?? ""
     })),
   });
 }
