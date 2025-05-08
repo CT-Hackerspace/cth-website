1e33eb37352779a179fcdd170b9792f3ee9844b4
Jack <allee.jack1@gmail.com>
2025-05-08 13:37:23 -0400

    New Build Script

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # if we can't find an agent, start one, and restart the script.
 if [ -z "$SSH_AUTH_SOCK" ] ; then
-  exec ssh-agent bash -c "ssh-add ; $0"
+  exec ssh-agent bash -c "ssh-add /home/.ssh/id_ec25519; $0"
   exit
 fi
-mkdir /tmp/build-site/
-cd /home/www//build-site/
+[ -d "/tmp/build-site/" ] || mkdir /tmp/build-site/
+cd /tmp/build-site/
 [ -d 'cth-website' ] || $(git clone git@github.com:CT-Hackerspace/cth-website.git)
 cd "cth-website"
 git pull origin
